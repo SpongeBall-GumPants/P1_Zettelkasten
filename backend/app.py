40880e18ab2aebcda896f4c7ced100909855fa92
SpongeBall <emreefeserin24@gmail.com>
2025-07-18 21:17:39 +0300

    chore(app): include authentication router in FastAPI app

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
+from sqlmodel import SQLModel, create_engine
+import models   # noqa: F401
+from authentication import router as auth_router
+
 
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -19,3 +23,13 @@ app.add_middleware(
 @app.get("/ping")
 async def ping():
     return {"pong": True}
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+
+app.include_router(auth_router)
